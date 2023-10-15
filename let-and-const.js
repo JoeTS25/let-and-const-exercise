5f15f2c73f11af36f3e54eb9482ad6a6a23ab2f7
JoeTS25 <141584648+JoeTS25@users.noreply.github.com>
2023-10-15 12:04:19 -0400

    Add files via upload

---
+++ let-and-const.js
@@ -0,0 +1,17 @@
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+/* Write an ES2015 Version */
+const PI = 3.14
+
+/* Question answers
+1. The difference between var and let is that var can be reassigned AND redeclared,
+but let can only be reassigned but not redeclared.
+
+2. The difference between var and const is that var can be reassigned AND redeclared,
+but const cannot be reassigned nor redeclared.
+
+3. The difference between let and const is that let can only be reassigned but not redeclared, 
+but const cannot be reassigned nor redeclared.
+
+4. Hoisting explains that metaphorically, variables are set at the top of the page and 
+you can access its undefined value before anything else.  Only works with var.*/
